551fd3fe24fa306179d99f21e821b399fbfc7d41
Ivan Febrianto <febriantoivan12@gmail.com>
2022-04-27 14:10:49 +0700

    finish the project

--- script.js
+++ script.js
@@ -6,33 +6,46 @@ function hasil(a, b){
 			return 0;
 		}
 		else if(b==1){
-			return 1;
+			return 2;
 		}
 		else{
-			return 2;
+			return 1;
 		}
 	}
 	else if(a==1){
 		if(b==0){
-			return 2;
+			return 1;
 		}
 		else if(b==1){
 			return 0;
 		}
 		else{
-			return 1;
+			return 2;
 		}
 	}
 	else{
 		if(b==0){
-			return 1;
+			return 2;
 		}
 		else if(b==1){
-			return 2;
+			return 1;
 		}
 		else{
 			return 0;
 		}
+	}
+}
+
+function ubah(pilihan){
+	pilihan=pilihan.toLowerCase();
+	if(pilihan[0]=='r'){
+		return 0;
+	}
+	else if(pilihan[0]=='p'){
+		return 1;
+	}
+	else{
+		return 2;
 	}
 }
 
@@ -47,9 +60,36 @@ function playRound(playerSelection, computerSelection){
 		return "Draw!";
 	}
 	else if(result==1){
-		return `You Win! ${arr[playerSelection]} beats ${arr[computerSelection]}`;
+		return `You Win! ${arr[playerSelection]} beats ${arr[computerSelection]}.`;
 	}
 	else{
-		return `You Lose! ${arr[computerSelection]} beats ${arr[playerSelection]}`;
+		return `You Lose! ${arr[computerSelection]} beats ${arr[playerSelection]}.`;
 	}
 }
+
+function game(){
+	let playerScore=0, computerScore=0;
+	for(let i=1;i<=5;++i){
+		let playerSelection=ubah(prompt("Please Enter `Rock, Paper, or Scissors`"));
+		let computerSelection=computerPlay(); 
+		let result=playRound(playerSelection, computerSelection);
+		console.log(result);
+		if(result[4]=='W'){
+			playerScore++;
+		}
+		else if(result[4]=='L'){
+			computerScore++;
+		}
+	}
+	if(playerScore > computerScore){
+		return "Congrats! You Won This Round!";
+	}
+	else if(playerScore == computerScore){
+		return "This Round Result is Draw!";
+	}
+	else{
+		return "Sorry, You Lost this Round.";
+	}
+}
+
+console.log(game());
